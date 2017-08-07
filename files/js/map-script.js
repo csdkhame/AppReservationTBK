db290b74e7fa31b647e3e4ff2f682fb29d251d83
ozamanJS <ozaclever@hotmail.com>
2017-08-07 12:38:12 +0700

    c

--- files/js/map-script.js
+++ files/js/map-script.js
@@ -1,4 +1,3 @@
-
 $('#boxRealtime input').focus(function() {
     $('#chk_val_boxsearch').val(1);
     $('#chk_val_boxsearch').addClass(this.id);
@@ -217,9 +216,9 @@ function initAutocomplete(map, start, end) {
 
             geocoder.geocode({ 'location': latlng }, function(results, status) {
                 var placeId = results[1].place_id;
-//                sendplaceid(placeId);
-//                console.log(results)
-				var num =  results.length;
+                //                sendplaceid(placeId);
+                //                console.log(results)
+                var num = results.length;
                 console.log(results[4].address_components[0].long_name);
                 if (status === google.maps.GeocoderStatus.OK) {
                     if (results[1]) {
@@ -297,13 +296,238 @@ function initAutocomplete(map, start, end) {
                 //return dist
             $.ajax({
                 type: 'POST',
-                url: 'https://dotdotdottrip.com/laglng_control/getlaglng',
+                url: '../service/servicereltime.php',
                 data: { 'lat_f': lat_f, 'lng_f': lng_f, 'distance': dist, 'lat_t': lat_t, 'lng_t': lng_t },
                 //contentType: "application/json",
                 dataType: 'json',
                 success: function(data) {
-                    console.log(data)
+                    console.log(data.length)
+                    if (data != null) {
+                        $('#ul-header2').css('display', 'block');
+                        $('#container-product').css('display', 'block');
+                        // $('#pro-search').css('display', 'block');
+                        $('#loading').css('display', 'block');
+                        //$('#content').css('display','none');
+                        setTimeout(function() {
+                            //$('#search-raeltime').css('display', 'none');
+                            // $('#map').css('display', 'none');
+                            $('#loading').css('display', 'none');
+                            $('#show-hide-pro').css('display', 'block');
 
+                            $("#pro-search").slideDown('4000'); //fadeIn(4000);
+                            // $('#search-show').css('display', 'block')
+                            //$('#content').css('display','block'); 
+                        }, 2000);
+
+                        data1 = data.data1;
+                        var cartype = data.cartype;
+                        console.log(data1)
+                        console.log(cartype)
+
+                        $.each(cartype, function(i, val) {
+                            var index2 = parseInt(i) + 1;
+                            $('#cartype').append('<option value="' + cartype[i] + '" label="' + cartype[i] + '" none=""></option>');
+                            //dataProvince.push(data[i])
+                            //$('#select-name').append('<li id="ct'+data[i].phonecode+'" value="'+data[i].phonecode+'" dataname ="'+data[i].name_en+'" img="'+data[i].country_code+'" onclick="sendCountry('+data[i].phonecode+');"><img id="imgcountry" src="'+url+'files/img/flag/icon/'+data[i].country_code+'.png'+'">'+'<span id="span-phonecode">('+'+'+data[i].phonecode+')</span>'+data[i].name_en+'</li>');
+
+
+                        });
+                        $.each(data1, function(i, val) {
+                                if (data1[i].type == 'Private') {
+                                    compae1private.push(data1[i])
+                                }
+
+                                if (data1[i].type == 'Join') {
+                                    compae1join.push(data1[i])
+                                }
+
+                            })
+                            // $.each(data2, function(i, val) {
+                            //     if (data2[i].type == 'Private') {
+                            //         compae2private.push(data2[i])
+                            //     }
+
+                        //     if (data2[i].type == 'Join') {
+                        //         compae2join.push(data2[i])
+                        //     }
+
+                        // })
+                        console.log(compae2private.length)
+                            //if (true) {}
+                            // console.log(data2[0])
+                            // console.log(data[1])
+                            // console.log(data[2])
+                            // console.log(data[3])
+                            // console.log(data[4])
+                            // $.each(data, function(i, item) {
+                            //       console.log(data[2])
+                            // });
+                        console.log(data.length)
+                        dataproduct = data;
+                        var urlicon = 'https://dotdotdottrip.com/files/images/carmodelicon/';
+
+                        //if (data[0].data1.length != 0) {
+                        var car_topic, cartype, pax;
+                        $.each(compae1private, function(i, val) {
+                            var indexs = parseInt(i) + 1;
+                            if ($.cookie("lng") == 'cn') {
+                                car_topic = compae1private[i].topic_cn;
+                                cartype = compae1private[i].car_topic_cn;
+                                pax = compae1private[i].pax_cn;
+                                $('.lng-book').html('預訂')
+                                $('.lng-capacity-info').html('容量信息')
+                                $('.lng-facilities').html('设施')
+                            } else if ($.cookie("lng") == 'en') {
+                                car_topic = compae1private[i].topic_en;
+                                cartype = compae1private[i].car_topic_en;
+                                pax = compae1private[i].pax_en;
+                                $('.lng-book').html('Book')
+                                $('.lng-capacity-info').html('Capacity info')
+                                $('.lng-facilities').html('Facilities')
+
+                            } else if ($.cookie("lng") == 'th') {
+                                car_topic = compae1private[i].topic_th;
+                                cartype = compae1private[i].car_topic_th;
+                                pax = compae1private[i].pax_th;
+                                $('.lng-book').html('จอง')
+                                $('.lng-capacity-info').html('ข้อมูลความจุ')
+                                $('.lng-facilities').html('สิ่งอำนวยความสะดวก ')
+
+                            } else if ($.cookie("lng") == undefined) {
+                                car_topic = compae1private[i].topic_en;
+                                cartype = compae1private[i].car_topic_en;
+                                pax = compae1private[i].pax_en;
+                                $('.lng-book').html('Book')
+                                $('.lng-capacity-info').html('Capacity info')
+                                $('.lng-facilities').html('Facilities')
+
+                            }
+                            console.log(compae1join.length)
+
+                            $('#product_a').append('<div class="a-link-item col-lg-12" >' +
+                                '<div class="item-thumbnail2" onclick="gotransfer(\'' + val + '\')">' +
+                                '<img src="' + urlicon + compae1private[i].transfer_icon + '.jpg">' +
+                                '</div>' +
+                                '<h2 class="searchresult_name" title="product name">' +
+                                '<span >' + car_topic + '</span>' +
+                                '<span class="hotel_num">' + indexs + '</span>' +
+                                '</h2>' +
+                                '<div class="box-province">' +
+                                '<p class="type-t">' +
+                                '<span class="car-type" >' + cartype + pax + '</span>' +
+                                '</p>' +
+
+                                '</div>' +
+                                '<div id="box-cost-view">' +
+                                '<div class="product_r">' +
+                                '<span class="base_price"></span>' +
+                                '<span class="sala">' + compae1private[i].cost_a_agent_all.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '฿' + '</span>' +
+
+                                '</div>' +
+                                '<div class="views-item">' +
+                                '<a  href="book?data=' + compae1private[i].transfer_id + '&from=' + id_placefrom + '&to=' + id_placeto + '" > <span class="lng-book"></span></a>' +
+
+                                '</div>' +
+                                '</div>' +
+                                '<div id="i-list"   onclick="getcondition(\'' + compae1private[i].car_model + '\')">' +
+                                '<p id="capacity"><span class="lng-capacity-info"></span></p>' +
+                                '<i class="fa fa-list-alt"   aria-hidden="true"></i>' +
+                                '</div>' +
+                                '</div>'
+
+                            );
+
+
+
+
+
+                        }); //end private
+
+                        var car_topic, cartype, pax;
+                        if (compae1join.length != 0) {
+                            $.each(compae1join, function(i, val) {
+                                var indexs = parseInt(i) + 1;
+                                if ($.cookie("lng") == 'cn') {
+                                    car_topic = compae1join[i].topic_cn;
+                                    cartype = compae1join[i].car_topic_cn;
+                                    pax = compae1join[i].pax_cn;
+                                    $('.lng-book').html('預訂')
+                                    $('.lng-capacity-info').html('容量信息')
+                                    $('.lng-facilities').html('设施')
+                                } else if ($.cookie("lng") == 'en') {
+                                    car_topic = compae1join[i].topic_en;
+                                    cartype = compae1join[i].car_topic_en;
+                                    pax = compae1join[i].pax_en;
+                                    $('.lng-book').html('Book')
+                                    $('.lng-capacity-info').html('Capacity info')
+                                    $('.lng-facilities').html('Facilities')
+
+                                } else if ($.cookie("lng") == 'th') {
+                                    car_topic = compae1join[i].topic_th;
+                                    cartype = compae1join[i].car_topic_th;
+                                    pax = compae1join[i].pax_th;
+                                    $('.lng-book').html('จอง')
+                                    $('.lng-capacity-info').html('ข้อมูลความจุ')
+                                    $('.lng-facilities').html('สิ่งอำนวยความสะดวก ')
+
+                                } else if ($.cookie("lng") == undefined) {
+                                    car_topic = compae1join[i].topic_en;
+                                    cartype = compae1join[i].car_topic_en;
+                                    pax = compae1join[i].pax_en;
+                                    $('.lng-book').html('Book')
+                                    $('.lng-capacity-info').html('Capacity info')
+                                    $('.lng-facilities').html('Facilities')
+                                }
+                                $('#product_c').append('<div class="a-link-item col-lg-12" >' +
+                                    '<div class="item-thumbnail2" onclick="gotransfer(\'' + val + '\')">' +
+                                    '<img src="' + urlicon + compae1join[i].transfer_icon + '.jpg">' +
+                                    '</div>' +
+                                    '<h2 class="searchresult_name" title="product name">' +
+                                    '<span >' + car_topic + '</span>' +
+                                    '<span class="hotel_num">' + indexs + '</span>' +
+                                    '</h2>' +
+                                    '<div class="box-province">' +
+                                    '<p class="type-t">' +
+                                    '<span class="car-type" >' + cartype + pax + '</span>' +
+                                    '</p>' +
+
+
+                                    '</div>' +
+                                    '<div id="box-cost-view">' +
+                                    '<div class="product_r">' +
+                                    '<span class="base_price"></span>' +
+                                    '<span class="sala">' + compae1join[i].cost_a_agent_all.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") + '฿' + '</span>' +
+
+                                    '</div>' +
+                                    '<div class="views-item" >' +
+                                    '<a  href="book?data=' + compae1join[i].transfer_id + '&from=' + id_placefrom + '&to=' + id_placeto + '" > <span class="lng-book"></span></a>' +
+
+                                    '</div>' +
+                                    '</div>' +
+                                    '<div id="i-list"   onclick="getcondition(\'' + compae1join[i].car_model + '\')">' +
+                                    '<p id="capacity"><span class="lng-capacity-info"></span></p>' +
+                                    '<i class="fa fa-list-alt"   aria-hidden="true"></i>' +
+                                    '</div>' +
+                                    '</div>'
+
+                                );
+
+
+                            });
+                        } else {
+                            $('#product_c').append('<div class="not-found">Product not Found</div>');
+                        }
+
+
+
+                    }
+
+
+
+
+                },
+                error: function(err) {
+                    console.log(err)
                 }
             });
             var infowindowDetailTravel = new google.maps.InfoWindow({ maxWidth: 200 });
