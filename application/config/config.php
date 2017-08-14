f022015cf6992542d2b571b77ca087846286d2a8
5540011055 <izudsuarez@gmail.com>
2017-08-14 18:05:59 +0700

    nut2

--- application/config/config.php
+++ application/config/config.php
@@ -22,8 +22,8 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://dotdotdottrip.com/';
- //$config['base_url'] = 'http://localhost/AppReservationTBK/';
+//$config['base_url'] = 'https://dotdotdottrip.com/';
+ $config['base_url'] = 'http://localhost/AppReservationTBK/';
 // $config['base_url'] = '';
 
 /*
