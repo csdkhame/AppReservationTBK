5d9cfa5010327bdc443193d42d790d8e438385df
ozamanJS <ozaclever@hotmail.com>
2017-08-30 11:33:46 +0700

    c

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -328,7 +328,25 @@
                 <div style="padding: 12px;">
                    <input type="" name="" value="" id="idimage">
 
-                    
+                   <script>
+    jssor_slider1_init = function (containerId) {
+        var options = { $AutoPlay: 1 };
+        var jssor_slider1 = new $JssorSlider$(containerId, options);
+    };
+</script>
+<div id="slider1_container" style="position: relative; top: 0px; left: 0px; width: 600px; height: 300px;">
+                            <!-- Slides Container -->
+    <div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width: 600px; height: 300px;">
+    <div><img u="image" src="<?php echo base_url(); ?>files/img/index-01.jpg" /></div>
+    <div><img u="image" src="<?php echo base_url(); ?>files/img/index-02.jpg" /></div>
+    <div><img u="image" src="<?php echo base_url(); ?>files/img/index-03.jpg" /></div>
+    <div><img u="image" src="<?php echo base_url(); ?>files/img/index-04.jpg" /></div>
+        <div><img u="image" src="<?php echo base_url(); ?>files/img/index-05.jpg" /></div>
+        
+    </div>
+    <!-- Trigger -->
+    <script>jssor_slider1_init('slider1_container');</script>
+</div>
                     
                 </div>
                 
