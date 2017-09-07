29580a9aad6ca587fbb0ddfec7e0aaa164a040e6
ozamanJS <ozaclever@hotmail.com>
2017-09-07 17:18:47 +0700

    c

---
+++ application/views/payment.php
@@ -0,0 +1,48 @@
+<section style="    height: 100vh;
+    background: #fff;">
+            <div class="container">
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                            <fieldset>
+                                <input type="hidden" name="cmd" value="_xclick" />
+                                <input type="hidden" name="business" value="ozaclever-facilitator_api1.gmail.com" />
+                                <input type="hidden" name="lc" value="AU" />
+                                <input type="hidden" name="item_name" value="Payment" />
+                                <input type="hidden" name="item_number" value="P1" />
+                                <input type="hidden" name="currency_code" value="THB" />
+                                <input type="hidden" name="button_subtype" value="services" />
+                                <input type="hidden" name="no_note" value="0" />
+                                <input type="hidden" name="cn" value="Comments" />
+                                <input type="hidden" name="no_shipping" value="1" />
+                                <input type="hidden" name="rm" value="1" />
+                                <input type="hidden" name="return" value="https://dotdotdottrip.com/dashboard/payment/?payment=success" />
+                                <input type="hidden" name="cancel_return" value="https://dotdotdottrip.com/dashboard/payment/?payment=cancelled" />
+                                <input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHostedGuest" />
+                                <table style="    margin-top: 161px;">
+                                
+                                    <tr>
+                                        <td style="padding:0 5px 5px 0;">Amount
+                                        </td>
+                                        <td style="padding:0 5px 5px 0;">
+                                            <input type="text" name="amount" maxlength="200" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding:0 5px 5px 0;">
+                                            <input type="hidden" name="on0" value="Reference" />Reference
+                                        </td>
+                                        <td style="padding:0 5px 5px 0;"> 
+                                            <input type="text" name="os0" maxlength="200" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>&nbsp;</td>
+                                        <td style="padding:0 5px 5px 0;">
+                                            <input style="position:relative; background:#ffffff; border:0;" type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/pp-acceptance-medium.png" name="submit" alt="PayPal . The safer, easier way to pay online." />
+                                            <!-- <img alt="" style="border:0;" src="https://www.paypalobjects.com/en_AU/i/scr/pixel.gif" width="1" height="1" /> -->
+                                        </td>
+                                    </tr>
+                                </table>
+                            </fieldset>
+                        </form>
+                        </div>
+    </section>
